4637d1932298635ecf8791df698e123d5d010c11
mepolov <mepolov@uma.tech>
2025-05-22 22:17:24 +0300

    Rename API

--- main.py
+++ main.py
@@ -4,27 +4,35 @@ from fastapi.responses import JSONResponse
 import httpx
 import asyncio
 
-app = FastAPI()
+app = FastAPI(title="Aggregator API", version="1.0")
 
 PARSERS = {
-    "gorzdrav": "http://gorzdrav-parser:8000/search",
-    "zdravcity": "http://zdravcity-parser:8000/search"
+    "gorzdrav": {
+        "url": "http://gorzdrav-parser:8000/search",
+        "param": "text"
+    },
+    "zdravcity": {
+        "url": "http://zdravcity-parser:8000/search",
+        "param": "what"
+    }
 }
 
-async def fetch_parser_data(url: str, query: str):
+async def fetch_parser_data(url: str, param_name: str, query: str):
     try:
         async with httpx.AsyncClient(timeout=20.0) as client:
-            response = await client.get(url, params={"query": query})
+            response = await client.get(url, params={param_name: query})
             response.raise_for_status()
             return response.json()
     except Exception as e:
         print(f"❌ Ошибка запроса к {url}: {e}")
         return {"results": [], "error": str(e)}
 
-
 @app.get("/searchAndAggregate", summary="Общий поиск", tags=["Aggregator"])
-async def search_all(query: str = Query(...)):
-    tasks = [fetch_parser_data(url, query) for url in PARSERS.values()]
+async def search_all(query: str = Query(..., description="Поисковый запрос")):
+    tasks = [
+        fetch_parser_data(parser["url"], parser["param"], query)
+        for parser in PARSERS.values()
+    ]
     results = await asyncio.gather(*tasks)
 
     all_items = []
